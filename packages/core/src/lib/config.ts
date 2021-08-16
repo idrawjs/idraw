8b5995dfb3619e3cc96d16a70b89ac080f5ad9bc
chenshenhai <cshenhai@gmail.com>
2021-08-16 10:58:15 +0800

    feat: add e2e test for core's api

--- packages/core/src/lib/config.ts
+++ packages/core/src/lib/config.ts
@@ -5,7 +5,7 @@ const defaultConfig: TypeConfigStrict = {
   elementWrapper: {
     color: '#2ab6f1',
     lockColor: '#aaaaaa',
-    dotSize: 8,
+    dotSize: 6,
     lineWidth: 1,
     lineDash: [4, 3],
   }
