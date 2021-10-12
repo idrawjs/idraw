50410cf705a9f8d4bf6451fd61afb8e66dcda002
chenshenhai <cshenhai@gmail.com>
2021-10-12 23:25:16 +0800

    refactor: abandon shadow of element-circle

--- packages/core/src/lib/draw/circle.ts
+++ packages/core/src/lib/draw/circle.ts
@@ -1,7 +1,7 @@
 import { TypeContext, TypeElement, } from '@idraw/types';
-import util from '@idraw/util'
+// import util from '@idraw/util'
 import { rotateElement } from './../transform';
-import is from './../is';
+// import is from './../is';
 import { clearContext } from './base';
 
 export function drawCircle(ctx: TypeContext, elem: TypeElement<'circle'>) {
@@ -11,7 +11,7 @@ export function drawCircle(ctx: TypeContext, elem: TypeElement<'circle'>) {
     const {
       bgColor = '#000000',
       borderColor = '#000000',
-      borderWidth
+      borderWidth = 0,
     } = desc;
 
     const a = w / 2;
@@ -19,21 +19,11 @@ export function drawCircle(ctx: TypeContext, elem: TypeElement<'circle'>) {
     const centerX = x + a;
     const centerY = y + b;
     const unit = (a > b) ? 1 / a : 1 / b;
+
+    // draw border
     if (borderWidth && borderWidth > 0) {
       const ba = borderWidth / 2 + a;
       const bb = borderWidth / 2 + b;
-      if (desc.shadowColor !== undefined && util.color.isColorStr(desc.shadowColor)) {
-        ctx.setShadowColor(desc.shadowColor);
-      }
-      if (desc.shadowOffsetX !== undefined && is.number(desc.shadowOffsetX)) {
-        ctx.setShadowOffsetX(desc.shadowOffsetX);
-      }
-      if (desc.shadowOffsetY !== undefined && is.number(desc.shadowOffsetY)) {
-        ctx.setShadowOffsetY(desc.shadowOffsetY);
-      }
-      if (desc.shadowBlur !== undefined && is.number(desc.shadowBlur)) {
-        ctx.setShadowBlur(desc.shadowBlur);
-      }
       ctx.beginPath();
       ctx.setStrokeStyle(borderColor);
       ctx.setLineWidth(borderWidth)
@@ -45,6 +35,7 @@ export function drawCircle(ctx: TypeContext, elem: TypeElement<'circle'>) {
       ctx.stroke();
     }
 
+    // draw content
     ctx.beginPath();
     ctx.setFillStyle(bgColor);
     ctx.moveTo(centerX + a, centerY);
@@ -53,13 +44,39 @@ export function drawCircle(ctx: TypeContext, elem: TypeElement<'circle'>) {
     }
     ctx.closePath();
     ctx.fill();
+    
+    // // draw shadow
+    // clearContext(ctx);
+    // if ((desc.shadowOffsetX !== undefined && is.number(desc.shadowOffsetX)) || desc.shadowOffsetY !== undefined && is.number(desc.shadowOffsetY)) {
 
-    // ctx.beginPath();
-    // ctx.setFillStyle(color);
-    // ctx.arc(x + a, y + b, r, 0, 2 * Math.PI, false);
-    // ctx.closePath();
-    // ctx.fill();
+    //   if (desc.shadowColor !== undefined && util.color.isColorStr(desc.shadowColor)) {
+    //     ctx.setShadowColor(desc.shadowColor);
+    //   }
+    //   if (desc.shadowOffsetX !== undefined && is.number(desc.shadowOffsetX)) {
+    //     ctx.setShadowOffsetX(desc.shadowOffsetX);
+    //   }
+    //   if (desc.shadowOffsetY !== undefined && is.number(desc.shadowOffsetY)) {
+    //     ctx.setShadowOffsetY(desc.shadowOffsetY);
+    //   }
+    //   if (desc.shadowBlur !== undefined && is.number(desc.shadowBlur)) {
+    //     ctx.setShadowBlur(desc.shadowBlur);
+    //   }
 
-    // ctx.scale(1, 1);
+    //   const a = (w + borderWidth * 2) / 2;
+    //   const b = (h + borderWidth * 2) / 2;
+    //   const centerX = x + a - borderWidth;
+    //   const centerY = y + b - borderWidth;
+    //   const unit = (a > b) ? 1 / a : 1 / b;
+
+    //   ctx.beginPath();
+    //   ctx.setFillStyle('#ffffff6a');
+    //   ctx.moveTo(centerX + a, centerY);
+    //   for(var i = 0; i < 2 * Math.PI; i += unit) {
+    //     ctx.lineTo(centerX + a * Math.cos(i), centerY + b * Math.sin(i));
+    //   }
+    //   ctx.closePath();
+    //   ctx.fill();
+    // }
+
   })
 }
