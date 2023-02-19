250a3a1c89684e21ca76ff0336c734399461b0d0
chenshenhai <cshenhai@gmail.com>
2023-02-19 21:41:39 +0800

    fix: cancel changeDate when mouseup element

--- packages/board/src/lib/watcher.ts
+++ packages/board/src/lib/watcher.ts
@@ -1,36 +1,46 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { TypePoint } from '@idraw/types';
 import { BoardEvent, TypeBoardEventArgMap } from './event';
 import { TempData } from './watcher-temp';
 
 export class Watcher {
-
   private _canvas: HTMLCanvasElement;
   private _isMoving = false;
   // private _onMove?: TypeWatchCallback;
   // private _onMoveStart?: TypeWatchCallback;
   // private _onMoveEnd?: TypeWatchCallback;
   private _event: BoardEvent;
-  private _temp: TempData = new TempData;
+  private _temp: TempData = new TempData();
 
   constructor(canvas: HTMLCanvasElement) {
     this._canvas = canvas;
     this._isMoving = false;
     this._initEvent();
-    this._event = new BoardEvent;
+    this._event = new BoardEvent();
   }
 
-  on<T extends keyof TypeBoardEventArgMap >(name: T, callback: (p: TypeBoardEventArgMap[T]) => void): void {
+  on<T extends keyof TypeBoardEventArgMap>(
+    name: T,
+    callback: (p: TypeBoardEventArgMap[T]) => void
+  ): void {
     this._event.on(name, callback);
   }
 
-  off<T extends keyof TypeBoardEventArgMap >(name: T, callback: (p: TypeBoardEventArgMap[T]) => void): void {
+  off<T extends keyof TypeBoardEventArgMap>(
+    name: T,
+    callback: (p: TypeBoardEventArgMap[T]) => void
+  ): void {
     this._event.off(name, callback);
   }
 
   _initEvent(): void {
     const canvas = this._canvas;
     canvas.addEventListener('mousemove', this._listenHover.bind(this), true);
-    canvas.addEventListener('mousedown', this._listenMoveStart.bind(this), true);
+    canvas.addEventListener(
+      'mousedown',
+      this._listenMoveStart.bind(this),
+      true
+    );
     canvas.addEventListener('mousemove', this._listenMove.bind(this), true);
     canvas.addEventListener('mouseup', this._listenMoveEnd.bind(this), true);
     canvas.addEventListener('mouseleave', this._listenMoveEnd.bind(this), true);
@@ -38,12 +48,16 @@ export class Watcher {
     canvas.addEventListener('click', this._listenClick.bind(this), true);
     canvas.addEventListener('wheel', this._listenWheel.bind(this), true);
 
-    canvas.addEventListener('touchstart', this._listenMoveStart.bind(this), true);
+    canvas.addEventListener(
+      'touchstart',
+      this._listenMoveStart.bind(this),
+      true
+    );
     canvas.addEventListener('touchmove', this._listenMove.bind(this), true);
     canvas.addEventListener('touchend', this._listenMoveEnd.bind(this), true);
   }
 
-  _listenHover(e: MouseEvent|TouchEvent): void {
+  _listenHover(e: MouseEvent | TouchEvent): void {
     e.preventDefault();
     const p = this._getPosition(e);
     if (this._isVaildPoint(p)) {
@@ -54,14 +68,14 @@ export class Watcher {
     this._isMoving = true;
   }
 
-  _listenLeave(e: MouseEvent|TouchEvent): void {
+  _listenLeave(e: MouseEvent | TouchEvent): void {
     e.preventDefault();
     if (this._event.has('leave')) {
       this._event.trigger('leave', undefined);
     }
   }
 
-  _listenMoveStart(e: MouseEvent|TouchEvent): void {
+  _listenMoveStart(e: MouseEvent | TouchEvent): void {
     e.preventDefault();
     const p = this._getPosition(e);
     if (this._isVaildPoint(p)) {
@@ -74,8 +88,8 @@ export class Watcher {
     }
     this._isMoving = true;
   }
-  
-  _listenMove(e: MouseEvent|TouchEvent): void {
+
+  _listenMove(e: MouseEvent | TouchEvent): void {
     e.preventDefault();
     e.stopPropagation();
     if (this._event.has('move') && this._isMoving === true) {
@@ -85,8 +99,8 @@ export class Watcher {
       }
     }
   }
-  
-  _listenMoveEnd(e: MouseEvent|TouchEvent): void {
+
+  _listenMoveEnd(e: MouseEvent | TouchEvent): void {
     e.preventDefault();
     if (this._event.has('moveEnd')) {
       const p = this._getPosition(e);
@@ -107,7 +121,7 @@ export class Watcher {
     }
   }
 
-  _listenClick(e: MouseEvent|TouchEvent) {
+  _listenClick(e: MouseEvent | TouchEvent) {
     e.preventDefault();
     const maxLimitTime = 500;
     const p = this._getPosition(e);
@@ -115,20 +129,21 @@ export class Watcher {
     if (this._isVaildPoint(p)) {
       const preClickPoint = this._temp.get('prevClickPoint');
       if (
-        preClickPoint && (t - preClickPoint.t <= maxLimitTime)
-        && Math.abs(preClickPoint.x - p.x) <= 5
-        && Math.abs(preClickPoint.y - p.y) <= 5
+        preClickPoint &&
+        t - preClickPoint.t <= maxLimitTime &&
+        Math.abs(preClickPoint.x - p.x) <= 5 &&
+        Math.abs(preClickPoint.y - p.y) <= 5
       ) {
         if (this._event.has('doubleClick')) {
           this._event.trigger('doubleClick', { x: p.x, y: p.y });
         }
       } else {
-        this._temp.set('prevClickPoint', {x: p.x, y: p.y, t, })
+        this._temp.set('prevClickPoint', { x: p.x, y: p.y, t });
       }
     }
   }
 
-  _getPosition(e: MouseEvent|TouchEvent): TypePoint {
+  _getPosition(e: MouseEvent | TouchEvent): TypePoint {
     const canvas = this._canvas;
     let x = 0;
     let y = 0;
@@ -151,13 +166,12 @@ export class Watcher {
     const p = {
       x: x - canvas.getBoundingClientRect().left,
       y: y - canvas.getBoundingClientRect().top,
-      t: Date.now(),
+      t: Date.now()
     };
     return p;
   }
 
   private _isVaildPoint(p: TypePoint): boolean {
-    return ( p.x > 0 && p.y > 0);
+    return p.x > 0 && p.y > 0;
   }
-  
 }
