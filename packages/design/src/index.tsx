78b97b69afb5d111c7081f44b1bd297b7e1e4b7b
chenshenhai <cshenhai@gmail.com>
2023-05-21 17:26:08 +0800

    feat: init sketch layout

--- packages/design/src/index.tsx
+++ packages/design/src/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Sketch } from './modules';
 import './css/index.less';
 
-export const Lab = () => {
+export const Design = () => {
+  const [width, setWidth] = useState<number>(1000);
+  const [height, setHeight] = useState<number>(600);
+
+  // const [width, setWidth] = useState<number>(window.innerWidth);
+  // const [height, setHeight] = useState<number>(window.innerHeight);
+  // useEffect(() => {
+  //   window.addEventListener('resize', () => {
+  //     const width = window.innerWidth;
+  //     const height = window.innerHeight;
+  //     setWidth(width);
+  //     setHeight(height);
+  //   });
+  // }, []);
+
   return (
-    <div style={{ position: 'fixed', left: 0, right: 0, width: '100%', height: '100%' }}>
-      <Sketch />
+    <div style={{ position: 'fixed', left: 0, right: 0, width, height }}>
+      <Sketch width={width} height={height} />
     </div>
   );
 };
