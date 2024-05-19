33d056512c9453816645fa89059f305428fe3542
chenshenhai <cshenhai@gmail.com>
2024-05-19 18:51:19 +0800

    pkgs: update packages

--- jest.config.js
+++ jest.config.js
@@ -30,7 +30,5 @@ module.exports = {
     '@idraw/util': '<rootDir>/packages/util/src/index.ts'
   },
   // "testRegex": "(/packages/idraw/__tests__/.*)\\.test.ts$",
-  setupFiles: [
-    // 'jest-canvas-mock'
-  ]
+  setupFiles: ['jest-canvas-mock']
 };
